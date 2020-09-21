f512562145c8c8b85b90e3d7775e29c5adc4603d
Teppei Torii <teppeichan9@gmail.com>
2020-09-21 16:09:14 +0900

    usersテーブルへのカラム追加

--- db/migrate/20200921064311_devise_create_users.rb
+++ db/migrate/20200921064311_devise_create_users.rb
@@ -32,7 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :password_confirmation, null: false
       t.timestamps null: false
     end
 
