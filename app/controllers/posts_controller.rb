8be45284b160b40a872d32e371f65146d6a21033
Teppei Torii <teppeichan9@gmail.com>
2020-09-22 11:34:13 +0900

    ヘッダーとフッターの部分テンプレート化、新規投稿画面の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
