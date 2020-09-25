7f1fd6acc40a2100a32bb081a356a204e95e98d3
Teppei Torii <teppeichan9@gmail.com>
2020-09-25 15:43:57 +0900

    ビューファイル変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :credit_card
+  belongs_to_active_hash :e_money
+  belongs_to_active_hash :code_pay
   with_options presence: true do
     validates :shop_name
     validates :credit_card_id
     validates :e_money_id
     validates :code_pay_id
   end
+  
 end
