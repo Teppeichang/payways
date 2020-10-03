cef6151eda2f8519164c9f2ff0dbd45fbfa77c6e
Teppei Torii <teppeichan9@gmail.com>
2020-10-03 20:34:57 +0900

    likeモデルの作成とアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  
-  validates :name, presence: true
-  validates :email, presence: true
-
   has_many :posts
   has_many :comments
   has_one_attached :image
+  has_many :likes
+
+  validates :name, presence: true
+  validates :email, presence: true
 
 end
