16c7f67276e22caf8a4682847a65a5f2c7c156c0
Teppei Torii <teppeichan9@gmail.com>
2020-10-03 23:13:43 +0900

    liked_by?メソッドの定義　等
    liked_by?メソッドの定義
    likesコントローラーの作成
    いいね/いいね済み　表示の作成
    動作確認ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
+  end
+
 end
