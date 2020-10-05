1f887719f1e297eea57049312b908179c6ef562b
Teppei Torii <teppeichan9@gmail.com>
2020-10-05 10:45:12 +0900

    デバッグ用記述の削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,6 @@ class PostsController < ApplicationController
 
   def create
     @post = PostsTag.new(post_params)
-    binding.pry
     if @post.valid?
       @post.save
       redirect_to root_path
