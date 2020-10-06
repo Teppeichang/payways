e41f2c8caaf422b127654418e42d1997054e1bb8
Teppei Torii <teppeichan9@gmail.com>
2020-10-06 23:12:07 +0900

    重複ファイルの削除　等
    devise.ja.ymlが重複していたため削除
    新規投稿・投稿編集ページにエラーメッセージが表示されるよう設定
    Postモデル単体テストコードの記述

---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    shop_name {Faker::Restaurant.name}
+    explain {Faker::Lorem.word}
+    association :user
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '店舗情報の投稿' do
+    it '写真とタグが入力されていなくても、店名と決済方式・お店に関する情報の両方が入力されていると投稿できる' do
+      expect(@post).to be_valid
+    end
+    it '店名が入力されていないと投稿できない' do
+      @post.shop_name = ""
+      @post.valid?
+      expect(@post.errors.full_messages).to include("店名を入力してください")
+    end
+    it '決済方式・お店に関する情報が入力されていないと投稿できない' do
+      @post.explain = ""
+      @post.valid?
+      expect(@post.errors.full_messages).to include("決済方式・お店に関する情報を入力してください")
+    end
+  end
+
+  describe '投稿の編集' do
+    it '写真とタグが入力されていなくても、店名と決済方式・お店に関する情報の両方が入力されていると投稿できる' do
+      expect(@post).to be_valid
+    end
+    it '店名が入力されていないと投稿できない' do
+      @post.shop_name = ""
+      @post.valid?
+      expect(@post.errors.full_messages).to include("店名を入力してください")
+    end
+    it '決済方式・お店に関する情報が入力されていないと投稿できない' do
+      @post.explain = ""
+      @post.valid?
+      expect(@post.errors.full_messages).to include("決済方式・お店に関する情報を入力してください")
+    end
+  end
+end
