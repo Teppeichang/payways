e129aed2598b282091572c710d70d067daa2d9b5
Teppei Torii <teppeichan9@gmail.com>
2020-10-07 00:25:57 +0900

    Commentモデルの単体テストコード記述とエラーメッセージが表示されるよう設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,13 @@
 class CommentsController < ApplicationController
   
   def create
-    comment = Comment.create(comment_params)
-    redirect_to post_path(comment.post.id)
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to post_path(comment.post.id)
+    else
+      flash[:alert] = "コメントを入力してください"
+      redirect_to post_path(comment.post.id)
+    end
   end
 
   private
