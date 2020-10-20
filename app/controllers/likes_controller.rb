2e8324026020a4cf8bd3188a13e88685bcd9bf2d
Teppei Torii <teppeichan9@gmail.com>
2020-10-20 20:16:14 +0900

    jQuery導入・初期設定
    併せて、いいね機能が一部非同期化できていなかったので修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,14 +7,12 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, post_id: params[:id])
     @post = Post.find_by(id: @like.post_id)
-    redirect_to post_path
   end 
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, post_id: params[:id])
     @post = Post.find_by(id: @like.post_id)
     @like.destroy
-    redirect_to post_path
   end
 
 end
