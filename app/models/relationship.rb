061a4bac68a66d60cedbd8a9068ec4ca4ef9c318
Teppei Torii <teppeichan9@gmail.com>
2020-11-03 11:53:35 +0900

    UserモデルとRelationshipモデルのアソシエーション設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,11 @@
 class Relationship < ApplicationRecord
+
+  # 自分をフォローしているユーザー（＝フォロワー）
+  belongs_to :follower, class_name: "User"
+  # 自分がフォローしているユーザー
+  belongs_to :following, class_name: "User"
+
+  validates :follower_id, presence: true
+  validates :following_id, presence: true
+  
 end
