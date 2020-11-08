e1f351f5256f2a6fbd29bd90e5edfe064e122db9
Teppei Torii <teppeichan9@gmail.com>
2020-11-08 17:34:13 +0900

    検索フォームにJavaScriptの動作を適用、検索結果ページのCSS調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery')
 require('./user_preview')
 require('./post_preview')
+require('./search_form')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
