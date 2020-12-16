243abdc22a548a24bcf4afcd20ca9058d4bf59c2
Teppei Torii <teppeichan9@gmail.com>
2020-12-16 20:49:41 +0900

    config.yml再記述、CircleCIで設定した環境変数の反映

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -54,3 +54,8 @@
 #     # password: "please use keys"
 #   }
 server '18.178.35.169', user: 'ec2-user', roles: %w[app db web]
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
