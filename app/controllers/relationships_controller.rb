ef2644908a600bf9ea577780e1e1150fcfdfd660
Teppei Torii <teppeichan9@gmail.com>
2021-02-05 13:28:11 +0900

    [fix]CircleCIテスト結果に基づくコード修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,6 @@
 class RelationshipsController < ApplicationController
+  before_action :set_search
+  
   def create
     @user = User.find(params[:following_id])
     current_user.follow(@user)
@@ -9,4 +11,11 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:id])
     current_user.unfollow(@user)
   end
+
+  private
+
+  def set_search
+    @q = Post.ransack(params[:q])
+  end
+
 end
